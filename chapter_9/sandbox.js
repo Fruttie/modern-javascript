e56d94b90314856f88f00d6542d51f5cc3408aa2
Fruttie <fruzsi.bacso@gmail.com>
2021-09-18 11:18:35 +0200

    Filter method

--- chapter_9/sandbox.js
+++ chapter_9/sandbox.js
@@ -1,7 +1,7 @@
 const scores = [10, 30, 15, 25, 50, 40, 5];
 
-// const highScores = scores.filter(score => score > 20);
-// console.log(highScores);
+// const filteredScores = scores.filter(score => score > 20);
+// console.log(filteredScores);
 
 const users = [
   {name: 'shaun', premium: true},
