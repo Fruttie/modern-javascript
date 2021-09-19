7f1c5553b18ac1a90f0e7fca6b5932758ccc43c7
Fruttie <fruzsi.bacso@gmail.com>
2021-09-19 22:31:29 +0200

    Todo filtering done

---
+++ todos/app.js
@@ -0,0 +1,61 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+const search = document.querySelector('.search input');
+
+const generateTemplate = (todo) => {
+
+    //template string
+    const html =  `
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+
+    list.innerHTML += html;
+
+};
+
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    //trim() method removes any kind of white space before and after the string
+    const todo = addForm.add.value.trim();
+
+    if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+    }
+    
+    
+});
+
+//delete todos
+
+list.addEventListener('click', e => {
+    if(e.target.classList.contains('delete')){
+        e.target.parentElement.remove();
+    }
+
+});
+
+
+const filterTodos = (term) => {
+
+    Array.from(list.children)
+        .filter((todo) => !todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.add('filtered'));
+
+        Array.from(list.children)
+        .filter((todo) => todo.textContent.toLowerCase().includes(term))
+        .forEach((todo) => todo.classList.remove('filtered'));
+};
+
+
+
+//keyup event
+search.addEventListener('keyup', () => {
+    const term = search.value.trim().toLowerCase();
+    filterTodos(term);
+})
