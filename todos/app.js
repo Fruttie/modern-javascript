688ef0d448101f24ec5e956409bfb57e7ae9eee4
Fruttie <fruzsi.bacso@gmail.com>
2021-09-19 20:10:57 +0200

    Adding data to Todo list

---
+++ todos/app.js
@@ -0,0 +1,31 @@
+const addForm = document.querySelector('.add');
+const list = document.querySelector('.todos');
+
+const generateTemplate = (todo) => {
+
+    //template string
+    const html =  `
+        <li class="list-group-item d-flex justify-content-between align-items-center text-light">
+            <span>${todo}</span>
+            <i class="far fa-trash-alt delete"></i>
+        </li>
+    `;
+
+    list.innerHTML += html;
+
+};
+
+addForm.addEventListener('submit', e => {
+
+    e.preventDefault();
+
+    //trim() method removes any kind of white space before and after the string
+    const todo = addForm.add.value.trim();
+
+    if(todo.length){
+    generateTemplate(todo);
+    addForm.reset();
+    }
+    
+    
+});
