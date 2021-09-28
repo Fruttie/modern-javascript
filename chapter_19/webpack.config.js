6dadc9d222bf431484c81a0a0f6045fcd9fcd61a
Fruttie <fruzsi.bacso@gmail.com>
2021-09-28 11:40:56 +0200

    setting up webpack

--- chapter_19/webpack.config.js
+++ chapter_19/webpack.config.js
@@ -1,9 +1,11 @@
+//this is a built in module in the node js core library
+//it specifies the path on tis computer (generalizes it for all computers)
 const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'dist/assets'),
     filename: 'bundle.js'
   }
 };
